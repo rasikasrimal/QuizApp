8832bba40b070cb1275dae02f69a958b355cef80
rasikasrimal <rasikacooldj620@gmail.com>
2023-07-09 21:21:49 +0530

    quiz app day 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/start_screen.dart';
 
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: StartScreen(),
+    ),
+  ));
+}
