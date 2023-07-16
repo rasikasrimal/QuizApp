665bcd37d5a987196c5e74d2ef791440505826d6
rasikasrimal <rasikacooldj620@gmail.com>
2023-07-16 18:54:02 +0530

    july 16

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/answer_button.dart';
 import 'package:flutter_application_1/data/questions.dart';
-import 'package:flutter_application_1/question_text.dart';
+//import 'package:flutter_application_1/question_text.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
